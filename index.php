159e872be8729f1733d3bfb8939e3d7cf19afb47
JEANRENAUN <JEANRENAUN@lab.ecinf.ch>
2018-08-30 11:00:31 +0200

    Debut du projet, cartouche et arboréscance

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Auteur: Jeanrenaud Nelson
+Titre: revision_forum
+Description : Un site d’échange d’information basique, une sorte de petit forum, vous permettant de poster des nouvelles.
+Version: 1.0.0
+Date: 30.08.2018 
+Copyright: Entreprise Ecole CFPT-I © 2018
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
